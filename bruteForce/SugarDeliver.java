c99b8642872b9ddf4344833da5455f4c38d80a9b
bswan0113 <sakulavi0113@gmail.com>
2024-01-22 16:54:32 +0900

    2839 SugarDeliver

--- bruteForce/SugarDeliver.java
+++ bruteForce/SugarDeliver.java
@@ -9,14 +9,25 @@ public class SugarDeliver {
             BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
             int input = Integer.parseInt(br.readLine());
             int result = -1;
+            int resultTemp1 = -1;
+            int resultTemp2 = -1;
 
             for (int i = 0; i <= input / 5; i++) {
                 int remaining = input - (i * 5);
                 if (remaining % 3 == 0) {
-                    result = i + (remaining / 3);
+                    resultTemp1 = i + (remaining / 3);
                     break;
                 }
             }
+            for (int i = 0; i <= input / 3; i++) {
+                int remaining = input - (i * 3);
+                if (remaining % 5 == 0) {
+                    resultTemp2 = i + (remaining / 5);
+                    break;
+                }
+            }
+            result = Math.min(resultTemp1, resultTemp2);
+
             bw.write(String.valueOf(result));
             br.close();
             bw.close();
